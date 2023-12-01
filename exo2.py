46e4b3f4156a11337dda3c5c8f2624fe1213f77a
Evking07 <150098930+Evking07@users.noreply.github.com>
2023-12-01 12:07:16 -0500

    exo2.py

--- exo2.py
+++ exo2.py
@@ -1,11 +1,18 @@
-def initListe(L,n):
-    for i in range(n):
-        L.append(i)
-       
-    return L    
 
-n=int(input("Entrez la valeur à ne pas atteindre : "))
-L=[]
-print(initListe(L,n))
+#q2
+def etoiles(n): 
+    '''int--->str'''
+    '''fonction Python récursive appelée etoiles qui prendra comme paramètre un entier non 
+    négatif et qui générera un dessin composé d’étoiles. 
+    Il affiche une série de triangle à l'endroit et à l'envers'''
 
+    #cas de base:
+    if (n==1):
+        print("*") 
+        print("*")
+    else:   #application de la récursibvité
+            print(n * "*")    
+            s= n * str(etoiles(n-1))
+            print(n * "*")           
+etoiles(4)
     
